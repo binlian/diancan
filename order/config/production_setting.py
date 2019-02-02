bddd73756ab1501f8c67d994a610b9c375f506c5
binglian <1016910233@qq.com>
2019-02-02 16:08:33 +0800

    first commit

---
+++ order/config/production_setting.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = 'mysql://http:vxDAawNKbcIwM3Ux@127.0.0.1/food_db?charset=utf8mb4'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = "utf8mb4"
+
+APP = {
+    'domain':'http://jiayougou.tp.54php.cn'
+}
+
+RELEASE_VERSION="20190123001"
